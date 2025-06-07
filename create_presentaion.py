fd9a6e9b584a35c0875ced7c996387ce9b6aad4e
Andrew Boateng <andrew.21@outlook.com>
2025-06-07 17:31:25 +0100

    Adds SlideSpeak API functionality

---
+++ create_presentaion.py
@@ -0,0 +1,38 @@
+import os
+import requests
+from typing import Union
+
+
+async def generate_presentaion(prompt: str) -> str:
+    url = "https://api.slidespeak.co/api/v1/presentation/generate"
+    headers = {
+        "Content-Type": "application/json",
+        "x-api-key": os.getenv(
+            "SLIDESPEAK_API_KEY"
+        ),  # Ensure you set this environment variable
+    }
+    payload = {
+        "plain_text": prompt,
+        "length": 2,
+        "template": "default",
+        "language": "ORIGINAL",
+        "fetch_images": True,
+        "tone": "default",
+        "verbosity": "standard",
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    return response.json().get('task_id')
+
+
+async def get_status(presentation_id: str) -> Union[dict, str]:
+    url = f"https://api.slidespeak.co/api/v1/task_status/{presentation_id}"
+    headers = {
+        "Content-Type": "application/json",
+        "x-api-key": os.getenv(
+            "SLIDESPEAK_API_KEY"
+        ),  # Ensure you set this environment variable
+    }
+
+    response = requests.get(url, headers=headers)
+    return response.json()
